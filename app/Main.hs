f11db85564b5e8963d9673ab9336cf92fab05bb8
Isaac Ross <isaac253@gmail.com>
2018-10-16 11:20:45 -0400

    updating to command parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,45 +1,83 @@
 module Main where
 
-import Options.Applicative
+import           Options.Applicative hiding (infoParser)
 
 type ItemIndex = Int
+
 type ItemDescription = Maybe String
 
-data Options = Options FilePath ItemIndex ItemDescription deriving Show
+data Options =
+  Options FilePath
+          Command
+  deriving (Show)
+
+data Command
+  = Info
+  | Init
+  | List
+  | Add
+  | View
+  | Update
+  | Remove
+  deriving (Show)
+
+infoParser :: Parser Command
+infoParser = pure Info
+
+initParser :: Parser Command
+initParser = pure Init
+
+listParser :: Parser Command
+listParser = pure List
+
+addParser :: Parser Command
+addParser = pure Add
+
+viewParser :: Parser Command
+viewParser = pure View
+
+updateParser :: Parser Command
+updateParser = pure Update
+
+removeParser :: Parser Command
+removeParser = pure Remove
+
+commandParser :: Parser Command
+commandParser =
+  subparser $
+  mconcat
+    [ command "info" (info infoParser (progDesc "Show Info"))
+    , command "init" (info initParser (progDesc "Initialize Items"))
+    , command "list" (info listParser (progDesc "List all Items"))
+    , command "add" (info addParser (progDesc "Add Item"))
+    , command "view" (info viewParser (progDesc "View Item"))
+    , command "update" (info updateParser (progDesc "Update Item"))
+    , command "remove" (info removeParser (progDesc "Remove Item"))
+    ]
 
 itemIndexParser :: Parser ItemIndex
 itemIndexParser = argument auto $ metavar "ITEMINDEX" <> help "index of item"
 
-
 updateItemDescriptionParser :: Parser ItemDescription
 updateItemDescriptionParser =
-  Just <$> itemDescriptionValueParser
-  <|> flag' Nothing (long "clear-desc")
-
+  Just <$> itemDescriptionValueParser <|> flag' Nothing (long "clear-desc")
 
 itemDescriptionValueParser :: Parser String
 itemDescriptionValueParser =
-  strOption (long "desc" <> short 'd' <> metavar "DESCRIPTION" <> help "description")
+  strOption
+    (long "desc" <> short 'd' <> metavar "DESCRIPTION" <> help "description")
 
 defaultDataPath :: FilePath
 defaultDataPath = "~/.to-do.yaml"
 
-
 optionsParser :: Parser Options
-optionsParser = Options
-  <$> dataPathParser
-  <*> itemIndexParser
-  <*> updateItemDescriptionParser
-
+optionsParser = Options <$> dataPathParser <*> commandParser
 
 dataPathParser :: Parser FilePath
-dataPathParser = strOption $
-  value defaultDataPath
-  <> long "data-path"
-  <> short 'p'
-  <> metavar "DATAPATH"
-  <> help ("path to data file (default " ++ defaultDataPath ++ ")")
-
+dataPathParser =
+  strOption $
+  value defaultDataPath <> long "data-path" <> short 'p' <> metavar "DATAPATH" <>
+  help ("path to data file (default " ++ defaultDataPath ++ ")")
 
 main :: IO ()
 main = do
